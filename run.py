b0e8ac51739e1adf088bcb07b2e8149dcc989182
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-24 20:59:17 +0300

    defining main function

--- run.py
+++ run.py
@@ -20,3 +20,27 @@ def delete_account(account):
     Function to delete an account
     '''
     account.delete_account()
+def display_accounts():
+    
+    return Account.display_accounts()
+
+def find_account(username):
+  
+    return Account.find_by_username(username)
+
+def check_existing_accounts(username):
+    
+    return Account.account_exist(username)
+
+print()
+print('PASSWORD LOCKER APPLICATION')
+print("")
+
+def main():
+    print("Hello welcome,Enter your name to create an account")
+    user_name=input()
+    print(f"Hello {user_name}. what would you like to do?")
+    print("\n")
+
+    while True:
+        print("Use these short codes : ca - create a new account,gp -generate new password ,cpg - get generated password by computor, da - display accounts,dp -display old password, fa -find an account, ex -exit the account list ")
