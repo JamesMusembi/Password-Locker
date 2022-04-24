a7185b41d10e906d5bf9cabd2cc8d7be96c8b70e
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-24 09:56:41 +0300

    created file

---
+++ users.py
@@ -0,0 +1,11 @@
+import unittest
+
+class User:
+    def __init__(self,fname,lname,password,email):
+        self.fname=fname
+        self.lname=lname
+        self.pasaword=password
+        self.email=email
+
+   
+print(User)
