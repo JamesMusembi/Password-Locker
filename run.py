913f158bef73069d52933d4e51dbdb8dd38887fe
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-24 21:27:02 +0300

    main function

--- run.py
+++ run.py
@@ -44,3 +44,67 @@ def main():
 
     while True:
         print("Use these short codes : ca - create a new account,gp -generate new password ,cpg - get generated password by computor, da - display accounts,dp -display old password, fa -find an account, ex -exit the account list ")
+        short_code=input().lower()
+        if short_code== 'ca':
+            print('New Account')
+            print('-'*15)
+
+            print('First Name....')
+            f_name=input()
+
+            print("Last Name....")
+            l_name=input()
+
+            print("Username ....")
+            username_=input()
+
+            print("Email ....")
+            email_=input()
+
+            print("Password ....")
+            password_=input()
+
+            save_accounts(create_account(f_name,l_name,username_,email_,password_)) # create and save new account.
+            print ('\n')
+            print(f"New account {f_name} {l_name} created")
+            print ('\n')
+
+        elif short_code=="da":
+            if display_accounts():
+                print("Here is the list of all your accounts..")
+                print("\n")
+
+                for account in display_accounts():
+                     print(f"{account.first_name} {account.last_name}{account.email} ...{account.username}")
+                     print('\n')
+
+            else:
+                print('\n')
+                print("You dont seem to have any accounts yet")
+                print('\n')
+
+        elif short_code=="fa":
+            print("Enter the username you want to search")
+            search_username=input()
+
+            if check_existing_accounts(search_username):
+                search_account = find_account(search_username)
+                print(f"{search_account.first_name} {search_account.last_name}")
+                print('-' * 20)
+
+                print(f"Username.......{search_account.username}")
+                print(f"Email address.......{search_account.email}")
+
+            else:
+                print("That contact does not exist")
+
+        elif short_code=="ex":
+            print("Thank you,byee.....")
+            break
+        
+        else:
+            print("I really didn't get that. Please use the short codes")
+
+
+if __name__=="__main__":
+    main()
