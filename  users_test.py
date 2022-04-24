f1e24f5d397630362379d02db7417fe1cb52d6b8
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-24 17:51:42 +0300

    creating test

---  users_test.py
+++  users_test.py
@@ -4,3 +4,10 @@ import unittest
 class AccountsTest(unittest.TestCase):
     def setup(self):
         self.new_account=Account("James", "Sembi", "Jamo" "jamo267", "james@gmail.com")
+
+    def test_init(self):
+        self.assertEqual(self.new_account.first_name,"James")
+        self.assertEqual(self.new_account.last_name,"Sembi")
+        self.assertEqual(self.new_account.username,"jamo")
+        self.assertEqual(self.new_account.password,"jamo267")
+        self.assertEqual(self.new_account.email,"james@gmail.com")
