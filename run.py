e3c98d753743728ac5cdbdbd220749700e2bfb1a
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-24 20:51:14 +0300

    creating account

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3.9
+
+# import email
+from credentials import Account
+
+
+def create_account(fname,lname,username,password,email):
+    '''
+    Function to create a new account
+    '''
+    new_account =Account(fname,lname,username,password,email)
+    return new_account
