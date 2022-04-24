38c175d9da802a3e313672b8edd0616ba62f8501
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-24 20:28:36 +0300

    fixing function

--- users.py
+++ users.py
@@ -1,6 +1,11 @@
- class Users:
-    """
-    Class that generates new instances of users
-    """
+ import unittest
 
-    pass
+class User:
+    def __init__(self,fname,lname,password,email):
+        self.fname=fname
+        self.lname=lname
+        self.pasaword=password
+        self.email=email
+
+   
+print(User)
