da0079e28862d27834e0ba300faf24a3124c6093
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-27 19:25:13 +0300

    fixing functionality

--- user_test.py
+++ user_test.py
@@ -13,7 +13,7 @@ class AccountsTest(unittest.TestCase):
         self.assertEqual(self.new_account.username,"jamo")
        
         # self.assertEqual(self.new_account.email,"jamo@gmail.com")
-        self.assertEqual(self.new_account.password,"jamo267")
+        self.assertEqual(self.new_account.password,"jamo@gmail.com")
     def test_save_account(self):
         self.new_account.save_account() # saving the new contact
         self.assertEqual(len(Account.account_list),1)
